8c30f24f9f4ad687d98a4b11b314d5009ca0b77a
Felice Pantaleo <felice.pantaleo@cern.ch>
2022-10-05 09:59:02 +0200

    fix line terminators

--- hands-on/floatingpoints/commutative_sum.cc
+++ hands-on/floatingpoints/commutative_sum.cc
@@ -6,8 +6,8 @@
 
 int main() {
 
-  int seed = time(NULL);
-     // Create vector v1 and fill it with a random uniform distribution:
+ int seed = time(NULL);
+   // Create vector v1 and fill it with a random uniform distribution:
   std::vector<double>  v1;
   std::uniform_real_distribution<double> unif(-1., 1.);
   // std::uniform_real_distribution<double> unif(-100000000.,100000000.);
@@ -15,14 +15,14 @@ int main() {
   for (size_t i = 0; i < 1000; ++i) {
     v1.push_back(unif(rng));
   }
-   // Duplicate v1
+  // Duplicate v1
 
-  // Sort v2 but not v1 
+ // Sort v2 but not v1 
 
-  // Compute the sums! 
+ // Compute the sums! 
 
-  //Compare the results
+ //Compare the results
 
-  return 0;
-   
+ return 0;
+  
 }
