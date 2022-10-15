6f2c716c368dc51345ddce67c813c01bfbac0758
Andres Vargas <vargasa@cua.edu>
2022-10-15 20:37:21 +0200

    CUDA excercise

--- hands-on/cuda_exercises/ex01/cuda_mem_model.cu
+++ hands-on/cuda_exercises/ex01/cuda_mem_model.cu
@@ -12,8 +12,9 @@
 // Here you can set the device ID that was assigned to you
 #define MYDEVICE 0
 
-__global__ void sum(const int* a, const int* b, int* c){
-  c[threadIdx.x ]
+__global__ void sum(const float* a, const float* b, float* c){
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    c[tid] = a[tid] + b[tid];
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -25,19 +26,19 @@ int main()
   CUDA_CHECK(cudaSetDevice(MYDEVICE));
 
   // Create a CUDA stream to execute asynchronous operations on this device
-  cudaStream_t stream;
-  CUDA_CHECK(cudaStreamCreate(&stream));
+  //cudaStream_t stream;
+  //CUDA_CHECK(cudaStreamCreate(&stream));
 
   // Pointer and dimension for host memory
-  size_t dimA = 8;
+  size_t dimA = 1024*2;
   std::vector<float> h_a(dimA);
-  //std::vector<float> h_b(dimA);
+  std::vector<float> h_b(dimA);
   std::vector<float> h_c(dimA);
 
   // Allocate and initialize host memory
   for (uint i = 0; i < dimA; ++i) {
      h_a[i] = i;
-     //h_b[i] = 2*i;
+     h_b[i] = 2*i;
   }
 
   // Pointers for device memory
@@ -49,6 +50,7 @@ int main()
 
   cudaMalloc(&d_a, memSize);
   cudaMalloc(&d_b, memSize);
+  cudaMalloc(&d_c, memSize);
 
   // CUDA_CHECK(cudaMallocAsync(___));
   // CUDA_CHECK(cudaMallocAsync(___));
@@ -57,43 +59,55 @@ int main()
   // Hint: the raw pointer to the underlying array of a vector
   // can be obtained by calling std::vector<T>::data()
   cudaMemcpy(d_a, h_a.data(), memSize, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b.data(), memSize, cudaMemcpyHostToDevice);
   
   //CUDA_CHECK(cudaMemcpyAsync(___));
 
   // Part 3 of 5: device to device memory copy
   //CUDA_CHECK(cudaMemcpyAsync(___));
-  cudaMemcpy(d_b, d_a, memSize, cudaMemcpyDeviceToDevice);
-
+  // cudaMemcpy(d_b, d_a, memSize, cudaMemcpyDeviceToDevice);
+  // cudaMemcpy(d_a, d_b, memSize, cudaMemcpyDeviceToDevice);
+  
   // Clear the host memory
   //std::fill(h_a.begin(), h_a.end(), 0);
 
   // Part 4 of 5: device to host copy
+  dim3 blocks(2,1,1); // define the number of blocks available as a 3d shape
+  //dim3 blocks();
+  //blocks.x = 1
+  sum<<<blocks /*number of blocks*/, 1024 /*threads within the block*/, 0>>>(d_a,d_b,d_c);
+  cudaDeviceSynchronize();
+  //cudaStreamSynchronize(stream);
   cudaMemcpy(h_c.data(),d_c, memSize, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+
+  for (int i = 0; i < dimA; ++i) {
+    std::cout << h_c[i] << '\n';
+    //assert(h_c[i] == 3.*i );
+  }
+
+
   //CUDA_CHECK(cudaMemcpyAsync(___));
 
   // Wait for all asynchronous operations to complete
   //CUDA_CHECK(cudaStreamSynchronize(stream));
-  sum<<<1, dimA, 0>>>(d_a,d_b,d_c);
 
   // Part 5 of 5: free the device memory
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
   //CUDA_CHECK(cudaFreeAsync(___));
   //CUDA_CHECK(cudaFreeAsync(___));
 
   // Verify the data on the host is correct
-  for (int i = 0; i < dimA; ++i) {
-    std::cout << h_c[i] << '\n';
-    //assert(h_a[i] == (float)i);
-  }
+
 
   // Destroy the CUDA stream
-  CUDA_CHECK(cudaStreamDestroy(stream));
+  //CUDA_CHECK(cudaStreamDestroy(stream));
 
   // If the program makes it this far, then the results are correct and
   // there are no run-time errors.  Good work!
-  std::cout << "Correct!" << std::endl;
+  //std::cout << "Correct!" << std::endl;
 
   return 0;
 }
